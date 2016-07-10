5c4772c3b2f2b26434d80eb9f2e2bb44270b8d90
YogeshwariJ <yogeshwarijayabalan6@gmail.com>
2016-07-10 12:32:02 +0530

    Update eleven.c

--- eleven.c
+++ eleven.c
@@ -1,15 +1,20 @@
-#include <stdio.h>
-int main()
+#include<stdio.h>
+#include<conio.h>
+void main()
 {
-    int n, i;
-
-    printf("Enter an integer: ");
-    scanf("%d",&n);
-
-    for(i=1; i<=10; ++i)
-    {
-        printf("%d * %d = %d \n", n, i, n*i);
-    }
-    
-    return 0;
+int n,x,p,i,j=1;
+clrscr();
+printf("Enter the n value:");
+scanf("%d",&n);
+printf("Enter the pair:");
+scanf("%d",&p);
+for(i=0;i<2*n;i++)
+{
+x=2*n-i;
+if(p!=x)
+{
+printf("The pair is%d for days %d is %d\n",p,j++,p);
+}
+}
+getch();
 }
